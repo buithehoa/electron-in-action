85b746a789062059b948091abc8c509b0ea96270
Bui The Hoa <buithehoa@gmail.com>
2019-01-24 15:29:46 +0700

    Disable nodeIntegration.

--- electronjs/first-app/main.js
+++ electronjs/first-app/main.js
@@ -6,13 +6,19 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
 
   // and load the index.html of the app.
   win.loadFile('index.html')
 
   // Open the DevTools.
   win.webContents.openDevTools()
+
+  win.maximize()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
