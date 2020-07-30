a393424ac3fa64fe184502136a8f1970701b59b6
Bui The Hoa <buithehoa@gmail.com>
2020-07-30 23:28:17 +0700

    Handle events.

--- electron/bookmarker/app/renderer.js
+++ electron/bookmarker/app/renderer.js
@@ -1,6 +1,38 @@
-const button = document.querySelector(".alert");
+const parser = new DOMParser();
 
-button.addEventListener('click', () => {
-  console.log("Click!");
-  alert(__dirname);
+const linksSection = document.querySelector('.links');
+const errorMessage = document.querySelector('.error-message');
+const newLinkForm = document.querySelector('.new-link-form');
+const newLinkUrl = document.querySelector('.new-link-url');
+const newLinkSubmit = document.querySelector('.new-link-submit');
+const clearStorageButton = document.querySelector('.clear-storage');
+
+newLinkUrl.addEventListener('keyup', () => {
+  newLinkSubmit.disabled = !newLinkUrl.validity.valid;
+});
+
+const parseResponse = (text) => {
+  return parser.parseFromString(text, "text/html");
+}
+
+const findTitle = (nodes) => {
+  return nodes.querySelector('title').innerText;
+}
+
+const clearForm = () => {
+  newLinkUrl.value = null;
+};
+
+const storeLink = (title, url) => {
+  localStorage.setItem(url, JSON.stringify({ title: title, url: url }));
+}
+
+newLinkForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const url = newLinkUrl.value;
+
+  fetch(url)
+    .then(response => response.text())
+    .then(parseResponse)
+    .then(findTitle);
 });
