715e98c0d0737d98fc134f01bdeb35c7336b3d38
Bui The Hoa <buithehoa@gmail.com>
2021-02-24 15:48:12 +0700

    Render index.html.

--- electron-in-action/firesale/app/main.js
+++ electron-in-action/firesale/app/main.js
@@ -4,7 +4,7 @@ let mainWindow = null;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow();
-  mainWindow.loadFile('index.html');
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   mainWindow.on('closed', () => {
     mainWindow = null;
