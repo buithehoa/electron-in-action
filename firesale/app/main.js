d4c9a6bec6b3784944e8b34e816a3523ed8fd622
Bui The Hoa <buithehoa@gmail.com>
2021-10-17 17:45:58 +0700

    Show window when resources are loaded.

--- firesale/app/main.js
+++ firesale/app/main.js
@@ -3,8 +3,12 @@ const { app, BrowserWindow } = require('electron');
 let mainWindow = null;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow();
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow = new BrowserWindow({ show: false });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
 
   mainWindow.on('closed', () => {
     mainWindow = null;
