b90927749eda12292ec40536309fc715440aa8ce
Bui The Hoa <buithehoa@gmail.com>
2021-10-17 18:06:54 +0700

    Display parsed Markdown on the fly.

--- firesale/app/main.js
+++ firesale/app/main.js
@@ -3,7 +3,15 @@ const { app, BrowserWindow } = require('electron');
 let mainWindow = null;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ show: false });
+  mainWindow = new BrowserWindow({
+    show: false,
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false
+    }
+  });
+
+
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   mainWindow.once('ready-to-show', () => {
