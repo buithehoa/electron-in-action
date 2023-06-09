7e2ce91b1cb6c0d8c8270fccb26fff97d3428821
Bui The Hoa <buithehoa@gmail.com>
2023-06-09 15:36:38 +0700

    Log opened files.

--- firesale/app/main.js
+++ firesale/app/main.js
@@ -23,3 +23,13 @@ app.on('ready', () => {
     mainWindow = null;
   });
 });
+
+const getFileFromUser = () => {
+  const files = dialog.showOpenDialog({
+    properties: ['openFile']
+  });
+
+  if (!files) return;
+
+  console.log(files);
+}
